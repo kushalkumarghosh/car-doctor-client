5621b31306242e15ca72cef85995496376314e00
kushalkumarghosh <cse.kushal1@gmail.com>
2024-04-03 19:26:34 +0600

    set basic layout for footer and header

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './Routes/Routes';
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
+);
