10de36ba152924e4ea4885bd964802c49de01946
kushalkumarghosh <cse.kushal1@gmail.com>
2024-04-04 17:28:53 +0600

    loaded services fake data and display service card

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,13 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
+import Services from "../Services/Services";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
-            <h2>This is home</h2>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(()=>{
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+    return (
+        <div className="mt-4">
+            <div className="text-center">
+                <h3 className="text-2xl font-bold text-orange-600">
+                    Our Services
+                </h3>
+                <h2 className="text-5xl">Our Service Area</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium <br/> quos nisi cum eaque qui pariatur eveniet explicabo quasi hic!</p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {
+                    services.map(service => <ServiceCard
+                    key={service._id}
+                    service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
