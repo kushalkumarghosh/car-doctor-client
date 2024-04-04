7ae49dae5284c64f2834a5675e0554a41d1a2936
kushalkumarghosh <cse.kushal1@gmail.com>
2024-04-04 15:10:35 +0600

    created about section with image relative position

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <About></About>
             <h2>This is home</h2>
         </div>
     );
