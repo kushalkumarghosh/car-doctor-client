847bc9e56d1fbdc1585c2025c07a2b81f6a72160
kushalkumarghosh <cse.kushal1@gmail.com>
2024-04-08 16:41:06 +0600

    updated

---
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,10 @@
+
+const CheckOut = () => {
+    return (
+        <div>
+            <h2>Book Service: </h2>
+        </div>
+    );
+};
+
+export default CheckOut;
--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
-  const {service_id, title, img, price } = service;
+  const {_id, title, img, price } = service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
@@ -11,7 +11,7 @@ const ServiceCard = ({ service }) => {
         <h2 className="card-title">{title}</h2>
         <p className="text-xl text-orange-500">Price: ${price}</p>
         <div className="card-actions">
-          <Link to={`/checkout/${service_id}`}>
+          <Link to={`/checkout/${_id}`}>
             <button className="btn btn-primary">Book Now</button>
           </Link>
         </div>
